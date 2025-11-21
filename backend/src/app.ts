a92f0fa191f4b5332cc118ab26c63b51bef84f89
Vidhi <mathurvidhi2505@gmail.com>
2025-11-21 19:43:12 +0530

    Commands updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,10 @@ import cors from 'cors';
 import noteRoutes from './routes/note-route';
 import { errorHandler } from './util/error';
 
-//
 export const app = express()
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "*"
 }))
 
 app.use(express.json())
--- backend/src/util/server.ts
+++ backend/src/util/server.ts
@@ -4,4 +4,13 @@ import { app } from "../app";
 
 dotenv.config();
 
-mongoose.connect(`${process.env.MONGODB_URI}`).then(() => app.listen(5000)).catch((err) => console.log(err))
+const uri = process.env.MONGODB_URI;
+if(!uri){
+    console.error("MONGODB_URI is missing");
+    process.exit(1);
+}
+
+mongoose.connect(uri).then(() => {
+    console.log("Mongo connected");
+    app.listen(5000, "0.0.0.0", () => console.log("Server on 5000"));
+}).catch(err => console.log(err))
